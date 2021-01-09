fe7ee61b9e784899290b562b415a88882ba55dbf
Antonio <antoniodainok@gmail.com>
2021-01-09 08:24:44 +0100

    window and canvas setup

--- main.py
+++ main.py
@@ -2,4 +2,21 @@
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
+# ---------------------------- UI SETUP ------------------------------
+from tkinter import *
+
+
+# window setup
+window = Tk()
+window.title("Password Managaer")
+
+
+# canvas setup
+canvas = Canvas(bd=0, height=200, width=200)
+image = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=image)
+canvas.grid(row=2, column=3, padx=20, pady=20)
+
+
+# run program
+window.mainloop()
